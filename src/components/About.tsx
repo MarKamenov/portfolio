e9d4d121c29840a5fdd2604080a7b45250bda900
Mariyan <mariyan.kamenov@nuffieldhealth.com>
2025-01-31 00:12:26 +0000

    chore: :construction: text update
    text update adding more info

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,8 +5,7 @@ const About: React.FC = () => {
     <section id="about" className={styles.about}>
       <div className="container">
         <h2>About Me</h2>
-        <p>I&apos;m a passionate front-end developer with a keen eye for creating engaging and responsive web experiences. With a strong foundation in TypeScript, React, React Native, Angular and modern web technologies, I strive to build user-friendly and visually appealing websites. Partial experience with Node.js/Express for backend development, with a focus on feature development and maintenance
-          tasks.</p>
+        <p>I&apos;m a passionate front-end developer with a keen eye for creating engaging and responsive web experiences. With a strong foundation in TypeScript, React, React Native, Angular and modern web technologies, I strive to build user-friendly and visually appealing web and mobile applications. I also have backend experience in Node.js & Express, allowing me to build full-stack applications.</p>
       </div>
     </section>
   );
